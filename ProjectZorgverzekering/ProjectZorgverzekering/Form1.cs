9b038076733cb03bfda93e53f5f0c1f150455729
nickglas <nickglas@hotmail.nl>
2018-06-21 15:59:06 +0200

    GUI

--- ProjectZorgverzekering/ProjectZorgverzekering/Form1.cs
+++ ProjectZorgverzekering/ProjectZorgverzekering/Form1.cs
@@ -16,5 +16,20 @@ namespace ProjectZorgverzekering
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
